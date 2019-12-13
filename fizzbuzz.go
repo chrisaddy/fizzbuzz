0e74ca921d25b877bc6c6c9d0a6004e7132070f2
chrisaddy <chris.william.addy@gmail.com>
2019-12-13 16:57:21 -0500

    init commit

---
+++ fizzbuzz.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"sort"
+	"strconv"
+	"time"
+)
+
+type Maybe interface {
+	Return(value interface{}) Maybe
+	Bind(func(interface{}) Maybe) Maybe
+}
+
+func contains(s []string, term string) bool {
+	i := sort.SearchStrings(s, term)
+	return i < len(s) && s[i] == term
+}
+
+func fizzbuzz(count int) string {
+	if count%15 == 0 {
+		return "FIZZBUZZ"
+	}
+	if count%3 == 0 {
+		return "FIZZ"
+	}
+
+	if count%5 == 0 {
+		return "BUZZ"
+	}
+
+	return strconv.Itoa(count)
+}
+
+func runner(mood []string) {
+	sort.Strings(mood)
+	i := 1
+	for {
+		fmt.Println(fizzbuzz(i))
+		if contains(mood, "gentle") {
+			time.Sleep(time.Second)
+
+		}
+		if contains(mood, "scatter-brained") {
+			i = rand.Intn(10000)
+		}
+		if contains(mood, "impatient") {
+			if i == 100 {
+				return
+			}
+		}
+		i += 1
+	}
+
+	return
+}
+
+func main() {
+	mood := os.Args[1:]
+	runner(mood)
+}
